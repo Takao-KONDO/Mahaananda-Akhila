ae40f63034caa04f4d5d8f59904acdbe2a07ae70
AKHILA\kondoh <kondotak@mta.biglobe.ne.jp>
2018-04-17 00:27:36 +0900

    レスポンシブ版のメニューがスクロール時に閉じてしまっていたのを改善

--- public_html/common/js/common_bs.js
+++ public_html/common/js/common_bs.js
@@ -18,25 +18,46 @@ $(function(){
     //メニューのトグル
     var toggleBtn = $('#toggleBtn .btnLine');
     var gnavArea = $('#gnavArea');
+	var toggleIcon01 = $('#toggleBtn .glyphicon.glyphicon-menu-hamburger');
+	var toggleIcon02 = $('#toggleBtn .glyphicon.glyphicon-remove');
     var overFlag = '';
     toggleBtn.on("click", function(){
         gnavArea.toggleClass("show");
+		toggleIcon01.toggleClass("hide");
+		toggleIcon02.toggleClass("feed");
     });
     $('#gnavArea, #toggleBtn .btnLine').hover(function(){
              overFlag = true;
     }, function(){
              overFlag = false;
     });
+	/*
     $('body').click(function() {
         if (overFlag === false && gnavArea.css('display') === 'block' ) {
-                gnavArea.removeClass("show");
+                //gnavArea.removeClass("show");
         }
     });
+	*/
     $(window).on("resize orientationchange", function () {
         if(window.innerWidth >= 992) {
             gnavArea.addClass("show");
+			toggleIcon01.addClass("hide");
+			toggleIcon02.addClass("feed");
         } else if (window.innerWidth < 992) {
-            gnavArea.removeClass("show");
+            //gnavArea.removeClass("show");
+			//toggleIcon01.removeClass("hide");
+			//toggleIcon02.removeClass("feed");
+        }
+    });
+	$(window).on("scroll", function () {
+        if(window.innerWidth >= 992) {
+            gnavArea.addClass("show");
+			toggleIcon01.addClass("hide");
+			toggleIcon02.addClass("feed");
+        } else if (window.innerWidth < 992) {
+            //gnavArea.removeClass("show");
+			//toggleIcon01.removeClass("hide");
+			//toggleIcon02.removeClass("feed");
         }
     });
 
